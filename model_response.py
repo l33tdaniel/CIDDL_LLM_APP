7ca0bec16816ee9f3e20b095864fce086fa196c3
Daniel Neugent <danielneugent@icloud.com>
2024-07-26 22:59:34 -0500

    added in some sort of links

--- model_response.py
+++ model_response.py
@@ -1,5 +1,6 @@
 import ollama, sys, chromadb, os
 from dotenv import load_dotenv
+import json
 load_dotenv()
 
 def generate_response(query=""):
@@ -11,14 +12,24 @@ def generate_response(query=""):
 
     queryembed = ollama.embeddings(model=embed_model, prompt=query)['embedding']
 
-    relevantdocs = collection.query(query_embeddings=[queryembed], n_results=5)["documents"][0]
-    docs = "\n\n".join(relevantdocs)
-    modelquery = f"{query} - Answer that question using the following text as a resource: {docs}"
+    relevantdocs = collection.query(query_embeddings=[queryembed], 
+                                    n_results=3,
+                                    include=["documents", 'metadatas']
+                                    )
+    end_text = ""
+    for index, doc in enumerate(relevantdocs['documents']):
+        end_text += "ORIGINAL SOURCE LINK: " + str(relevantdocs['metadatas'][index]) + str(doc) + "\n\n"
 
-    stream = ollama.generate(model=main_model, prompt=modelquery, stream=True)
+    modelquery = f"{query} - Answer that question in a personable and conversationable manner using the following text as a resource: {end_text}"
+
+    stream = ollama.generate(model='llama3.1:latest', prompt=modelquery, stream=True)
     output = ""
     for chunk in stream:
         if chunk["response"]:
-            print(chunk['response'], end='', flush=True)
+            #print(chunk['response'], end='', flush=True)
             output += chunk['response']
+    links = ''
+    for index, link in enumerate(relevantdocs['metadatas'][0]):
+        links += link['source'] + ' '
+    output += f"\n\nReference Links: {links}\n\n"
     return output
